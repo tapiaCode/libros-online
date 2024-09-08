d333e8790bd8d76a40e2e68a20546bacfff3e236
Tapia <burbujeuky@gmail.com>
2024-09-08 00:35:36 -0400

    first commit

---
+++ js/index.js
@@ -0,0 +1,35 @@
+function openMenu() {
+  document.body.classList += " menu--open";
+}
+
+function closeMenu() {
+  document.body.classList.remove("menu--open");
+}
+
+function abrirModal(pdfUrl) {
+  var modal = document.getElementById("modal");
+  var pdfViewer = document.getElementById("pdf-viewer");
+
+  pdfViewer.src = pdfUrl;
+  modal.style.display = "block";
+
+  console.log("Abriendo modal con PDF:", pdfUrl);
+}
+
+function cerrarModal() {
+  var modal = document.getElementById("modal");
+  var pdfViewer = document.getElementById("pdf-viewer");
+
+  modal.style.display = "none";
+  pdfViewer.src = "";
+
+  console.log("Cerrando modal");
+}
+
+// Cerrar el modal si se hace clic fuera de él
+window.onclick = function (event) {
+  var modal = document.getElementById("modal");
+  if (event.target == modal) {
+    cerrarModal();
+  }
+};
